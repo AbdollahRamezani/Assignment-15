77eb0d4bc4146c32cd7a382c1c5dedfc9c6f7f59
Abdollah Ramezani <53383194+ramezani84@users.noreply.github.com>
2023-01-21 19:14:05 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,70 @@
 import arcade
-class Snake(arcade.Sprite):
-    def __init__(self, game):
-        super().__init__()
-        self.width = 30
-        self.height = 30
-        self.center_x = game.width // 2
-        self.center_y = game.width // 2
-        self.color = arcade.color.GREEN      
-        self.change_x = 0
-        self.change_y = 0
-        self.speed = 4
-        self.score = 0
-        self.status = False
-        self.body = []   #لیستی از دیکشنری ها که شامل ایکس و وای هست  
+from snake import Snake
+from apple import Apple    
+from pear import Pear
+from bomb import Bomb
 
-    def draw(self):  #دز اينجا از متد دراو پدر استفاده نكرديم وبا اورلودينگ يك تابع از خودمان تعريف كرديم
-    
-      for index,item in enumerate(self.body):
-              if index % 2 == 0:
-                 color=arcade.color.BLUE   
-              else:
-                  color=arcade.color.RED              
-      arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height, self.color)
-      for part in self.body:
-            arcade.draw_rectangle_filled(part["x"], part["y"], self.width, self.height, color)
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(width=500, height=500, title="Super Snake V1")
+        arcade.set_background_color(arcade.color.KHAKI)
+        self.food = Apple(self)
+        self.snake = Snake(self)
+        self.pear = Pear(self)
+        self.bomb = Bomb(self)
+        
+
+    def on_draw(self):
+        arcade.start_render()
+        self.food.draw()
+        self.pear.draw()
+        self.snake.draw() #این تابع دراو تابع پدر نیست و تابعی است که خودمان در کلاس اسنیک تعریف کردیم
+        self.bomb.draw()
+        arcade.draw_text(f'Score:{self.snake.score}', 400, 20, arcade.color.DARK_BLUE, 15, 10, "right", "arial")
+
+         
+        if (self.snake.score <= 0 and self.snake.status == True) or self.width < self.snake.center_x or  self.snake.center_x< 0 or \
+           self.height < self.snake.center_y or  self.snake.center_y < 0:
+                arcade.draw_rectangle_filled(0, 0, 1800, 1500, arcade.color.BLACK)
+                arcade.draw_text("GAME OVER", 110,250,arcade.color.YELLOW,33, font_name= "Arial")
+        arcade.finish_render()   
+
+    def on_update(self, delta_time: float):
+        self.snake.move()
+
+        if arcade.check_for_collision(self.snake, self.food):
+            self.snake.eat(self.food)             
+            self.food = Apple(self)  #آبجكت سيب كه در متد ايت در کلاس اسنیک حذف شده اينجا دوباره ميسازيم
+
+        if arcade.check_for_collision(self.snake, self.pear):
+            self.snake.eat_pear(self.pear)             
+            self.pear = Pear(self)     
+
+        if arcade.check_for_collision(self.snake, self.bomb):
+            self.snake.eat_bomb(self.bomb)             
+            self.bomb = Bomb(self)   
+
+        if arcade.check_for_collision(self.snake, self.snake): # بررسی برخورد مار با بدن خودش
+            arcade.draw_rectangle_filled(0, 0, 1800, 1500, arcade.color.BLACK)
+            arcade.draw_text("GAME OVER", 110,250,arcade.color.YELLOW,33, font_name= "Arial")              
             
 
-    def move(self):   
-        self.center_x += self.change_x * self.speed  #اگر جهت حركت مثبت باشد مكان نيز مثبت ميشود و اگر جهت حركت منفي باشد مكان نيز منفي ميشود
-        self.center_y += self.change_y * self.speed 
-        self.body.append({"x": self.center_x, "y": self.center_y})
-        if len(self.body) > self.score:
-           self.body.pop(0)
+    def on_key_release(self, symbol: int, modifiers: int):
+        if symbol== arcade.key.UP:
+            self.snake.change_x = 0
+            self.snake.change_y = 1
+        elif symbol== arcade.key.DOWN:
+            self.snake.change_x = 0
+            self.snake.change_y = -1
+        elif symbol== arcade.key.RIGHT:
+            self.snake.change_x = 1
+            self.snake.change_y = 0
+        elif symbol== arcade.key.LEFT:
+            self.snake.change_x = -1
+            self.snake.change_y = 0   
 
-    def eat(self ,food):
-        del food    #براي اينكه هر دفعه يك سيب جديد ساخته بشه اول آبجكت سيب حذف ميكنيم
-        self.score += 1
-        self.status = True
+             
 
-    def eat_pear(self, pear):
-        del pear
-        self.score += 2   
-        self.status = True
 
-    def eat_bomb(self, bomb):
-        del bomb
-        self.score -=1 
-        self.status = True
-       
-        
-        
+game = Game()
+arcade.run()        
